524d32e332bfa070fd03b09761fc0afb3c465aff
Choi Daejin <soyenze@gmail.com>
2011-11-17 22:15:45 +0900

    Created the package named `spellit`.

--- main.py
+++ main.py
@@ -1,39 +1,13 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-from google.appengine.ext import webapp
-from database import worddb
-from google.appengine.api import urlfetch
 import json
-import news
+from google.appengine.api import urlfetch
+from google.appengine.ext import webapp
+from spellit.database import worddb
+from spellit import news
 
-import re
-TOKEN_TYPE_RE = re.compile(r'''
-    ( ={2,10}Noun={2,10}[^=]+= ) |
-    ( ={2,10}Pronoun={2,10}[^=]+= ) |
-    ( ={2,10}Adjective={2,10}[^=]+= ) |
-    ( ={2,10}Adverb={2,10}[^=]+= ) |
-    ( ={2,10}Verb={2,10}[^=]+= ) |
-    ( ={2,10}Preposition={2,10}[^=]+= ) |
-    ( ={2,10}Conjunction={2,10}[^=]+= ) |
-    ( ={2,10}Interjection={2,10}[^=]+= )
-''', re.VERBOSE)
 
 class BaseHandler(webapp.RequestHandler):
-    
+
     def render(self, html, json):
         t = self.request.accept.best_match(['text/html', 'application/json'])
         self.response.headers['Vary'] = 'Accept'
@@ -48,30 +22,34 @@ class BaseHandler(webapp.RequestHandler):
 
 
 class WordListHandler(BaseHandler):
+
     def get(self):
         offset = self.request.get('offset', default_value=0)
         limit = self.request.get('limit', default_value=10)
-        
+
         offset = int(offset)
         limit = int(limit)
-        
+
         word_db_handle = worddb.WordDBInterface
-        
+
         words = list(word_db_handle.getWords(int(limit), int(offset)))
         self.render(html=words, json=words)
-        
+
+
 class MeaningHandler(BaseHandler):
+
     def get(self):
         word = self.request.get('word')
-        
-        url = 'http://en.wiktionary.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json'
+
+        url = ('http://en.wiktionary.org/w/'
+               'api.php?action=query&prop=revisions&rvprop=content&format=json')
         url = url + '&titles=' + str(word)
-    
+
         result = urlfetch.fetch(url)
-        
+
         if result.status_code != 200:
             return
-        
+
         obj = json.loads(result.content)
         pagedata = obj['query']['pages']
         pages = pagedata.keys()
@@ -80,34 +58,35 @@ class MeaningHandler(BaseHandler):
         if len(pages) == 0:
             # raise no result error
             return
-        
+
         strdata = pagedata[pages[0]]['revisions'][0][u'*']
         #self.response.out.write(strdata)
-        
+
         for match in TOKEN_TYPE_RE.finditer(strdata):
             realdata = strdata[match.start():match.end()-1].encode('utf-8')
             self.response.out.write(realdata)
             break
-        
-        
+
+
 class WordHandler(BaseHandler):
+
     def get(self):
         strwords = self.request.get('words')
         strwords = str(strwords)
-        
-        words = strwords.split('|')
-        
-        word_db_handle = worddb.WordDBInterface
-        map(word_db_handle.insert, words)
 
+        words = strwords.split('|')
+
+        word_db_handle = worddb.WordDBInterface
+        for word in words:
+            word_db_handle.insert(word)
 
 
 class CrawlWordHandler(BaseHandler):
-    def get(self):
 
+    def get(self):
         NM = news.NewsManager()
         result = NM.GetNewsWords(2) # 2: number of news
-        
+
         word_db_handle = worddb.WordDBInterface
         word_db_handle.insertWords(result)
 
@@ -117,3 +96,4 @@ application = webapp.WSGIApplication([('/words', WordListHandler),
                                       ('/privacy/insertword', WordHandler),
                                       ('/privacy/crawlword', CrawlWordHandler)],
                                      debug=True)
+
--- spellit/news.py
+++ spellit/news.py
@@ -4,13 +4,15 @@ Created on Nov 11, 2011
 @author: soyenze
 '''
 
-import lxml.html
-import lxml.cssselect
 import feedparser
 from google.appengine.api import urlfetch
+import lxml.html
+import lxml.cssselect
+
 
 FEED_URL = "http://rss.cnn.com/rss/edition.rss"
 NEWS_TAG = "div.cnn_strycntntlft > p"
+
 
 class NewsManager(object):
 
@@ -18,12 +20,11 @@ class NewsManager(object):
         self.wordFrequency = {}
 
     def GetNewsWords(self, maxlength=20):
-        
-        print 11
+
         retcount = 0
         feed = urlfetch.Fetch(FEED_URL)
         result = feedparser.parse(feed.content)
-        
+
         if 'entries' in result:
             for item in result['entries']:
                 if retcount >= maxlength:
@@ -31,29 +32,28 @@ class NewsManager(object):
                 if item and item['links'] and item['links'][0]:
                     sentences = list(self.crawlWords(item['links'][0]))
                     retcount += 1
-                
+
                 def extractWords(content):
                     words = content.split(' ')
-                    
+
                     def storeFrequncy( word ):
                         if word in self.wordFrequency:
                             self.wordFrequency[word] += 1
                         else:
                             self.wordFrequency[word] = 1
-                    
+
                     for word in words:
                         #ascword = word.encode('ascii', 'ignore')
                         word.strip()
                         if word.isalpha() and word.islower():
                             storeFrequncy(word)
-                
+
                 map(extractWords, sentences)
-        
+
         return self.wordFrequency
-        
-            
+
+
     def crawlWords(self, newslink):
-        print newslink['href']
         html = lxml.html.parse(newslink['href'])
         sel = lxml.cssselect.CSSSelector(NEWS_TAG)
         paras = sel(html)
@@ -61,4 +61,4 @@ class NewsManager(object):
         for p in paras:
             if p.text:
                 yield p.text
-        
+
